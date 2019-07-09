ad78729b7822db0847bbef541fb645cc3406aac4
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-07-09 15:52:51 +0530

    Create ques5.cpp

---
+++ ques5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number,interval1,interval2;
+    cin>>interval1;
+    cin>>interval2;
+    for(int i=interval1;i<interval2-1;i++)
+    {
+        //cout<<i;    
+        number=i+1;
+        if(number%2==0)
+        {
+            cout <<number<<" ";
+        }                     
+        }     
+    return 0;
+}
